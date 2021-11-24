44668fa14b92c4e2ddf88c401adfd792b020ba4e
rhkearns <ryankearns217@gmail.com>
2021-11-24 15:52:44 -0500

    add signup to urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
   path('boxes/<int:pk>/update', views.BoxUpdate.as_view(), name='boxes_update'),
   path('boxes/<int:pk>/delete', views.BoxDelete.as_view(), name='boxes_delete'),
   path('boxes/<int:box_id>/add_item', views.add_item, name='add_item'),
+  path('accounts/signup/', views.signup, name='signup'),
 
 ]
