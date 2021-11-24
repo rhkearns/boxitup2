330b312b661746da41a683d98c2b4d75577389eb
rhkearns <ryankearns217@gmail.com>
2021-11-24 13:35:57 -0500

    add home.html

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
 def home(request):
-  return HttpResponse('<h1>Hello World</h1>')
+  return render(request, 'home.html')
 
 def boxes_index(request):
   return render(request, 'boxes/index.html', {'boxes': boxes })
