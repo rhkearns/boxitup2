7c148ffe240a31e4bb950bdf9d3f2624d478c5d2
rhkearns <ryankearns217@gmail.com>
2021-11-24 13:57:59 -0500

    add update and delete to urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
   path('boxes/', views.boxes_index, name='boxes_index'),
   path('boxes/<int:box_id>/', views.boxes_detail, name='boxes_detail'),
   path('boxes/create/', views.BoxCreate.as_view(), name='boxes_create'),
+  path('boxes/<int:pk>/update', views.BoxUpdate.as_view(), name='boxes_update'),
+  path('boxes/<int:pk>/delete', views.BoxDelete.as_view(), name='boxes_delete'),
 
 ]
