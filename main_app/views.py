6510769450b79a6f6f9594418980c0a1fbfd5674
rhkearns <ryankearns217@gmail.com>
2021-11-24 15:55:45 -0500

    add signup functions

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from .models import Box
 from .forms import ItemForm
 
@@ -40,3 +42,17 @@ def add_item(request, box_id):
     new_item.box_id = box_id
     new_item.save()
   return redirect('boxes_detail', box_id=box_id)
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('boxes_index')
+    else:
+      error_message = 'Invalid sign up -- please try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'signup.html', context)
