7997ce89c3a6d9886a1749340bfebf56cd86fb95
rhkearns <ryankearns217@gmail.com>
2021-11-24 13:50:12 -0500

    add create to urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
   path('', views.home, name='home'),
   path('boxes/', views.boxes_index, name='boxes_index'),
   path('boxes/<int:box_id>/', views.boxes_detail, name='boxes_detail'),
-  
+  path('boxes/create/', views.BoxCreate.as_view(), name='boxes_create'),
+
 ]
