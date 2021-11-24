7973172e7906d71c5bff333130a47e9a578c3db9
rhkearns <ryankearns217@gmail.com>
2021-11-24 13:39:53 -0500

    create box model and inital migration

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
+SIZES = (
+  ('S', 'Small'),
+  ('M', 'Medium'),
+  ('L', 'Large'),
+  ('XL', 'Extra-Large')
+)
+
 # Create your models here.
+class Box(models.Model):
+  number = models.IntegerField()
+  size = models.TextField(
+    max_length=2,
+    choices=SIZES,
+    default=SIZES[1][0]
+  )
+  category = models.CharField(max_length=20)
+  isClosed = models.BooleanField(default=False)
+
+  def __str__(self):
+    return str(self.number)
