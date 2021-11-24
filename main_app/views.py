8ed2e9bec406d7950e3eb30fd4107b6c2a941d4e
rhkearns <ryankearns217@gmail.com>
2021-11-24 13:41:25 -0500

    import model

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-
+from .models import Box
 
 # Create your views here.
 def home(request):
   return render(request, 'home.html')
 
 def boxes_index(request):
+  boxes = Box.objects.all()
   return render(request, 'boxes/index.html', {'boxes': boxes })
