c4e147d12682eb47e1a179cd59b177576017b6f6
rhkearns <ryankearns217@gmail.com>
2021-11-24 15:46:18 -0500

    add login to home

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='home'),
+  path('', views.Home.as_view(), name='home'),
   path('boxes/', views.boxes_index, name='boxes_index'),
   path('boxes/<int:box_id>/', views.boxes_detail, name='boxes_detail'),
   path('boxes/create/', views.BoxCreate.as_view(), name='boxes_create'),
